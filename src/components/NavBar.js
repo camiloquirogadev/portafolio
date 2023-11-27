c2ac6d632c0176e0d2260a5c920447e59448a947
camilo <camilosolquiroga@gmail.com>
2023-11-26 22:50:41 -0300

    add font face

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,5 +1,9 @@
 import { useState,  useEffect } from "react";
 import { Navbar, Container} from "react-bootstrap";
+import logo from './assets/img/logo.svg';
+import navIcon1 from './assets/img/nav-icon1.svg';
+import navIcon2 from './assets/img/nav-icon2.svg';
+import navIcon3 from './assets/img/nav-icon3.svg';
 
 export const NavBar = () => {
     const [activeLink, setActiveLink] = useState('home');
@@ -39,9 +43,9 @@ export const NavBar = () => {
                         </Nav>
                         <span className="navbar-text">
                             <div className="social-icon">
-                                <a href="#"><img src={} alt="" /></a>
-                                <a href="#"><img src={} alt="" /></a>
-                                <a href="#"><img src={} alt="" /></a>
+                                <a href="#"><img src={navIcon1} alt="" /></a>
+                                <a href="#"><img src={navIcon2} alt="" /></a>
+                                <a href="#"><img src={navIcon3} alt="" /></a>
                             </div>
                             <button className="vvd" onClick={() => console.log('connect')}><span>Let's Connect</span></button>
                              </span>
