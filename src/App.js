6f63ca27cc94eda627cfb5ef846f9bf7417e5117
Camilo Quiroga <camilosolquiroga@gmail.com>
2023-12-05 13:41:16 -0300

    update banner

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
-import logo from './logo.svg';
+/*import logo from './logo.svg';*/
 import './App.css';
 import { NavBar } from './components/NavBar';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Banner } from 'c:/Users/csquiroga/Desktop/repo/protafolio/src/App.js';
+import { Banner } from './components/Banner';
+
 
 function App() {
   return (
     <div className="App">
      <NavBar />
      <Banner />
+
     </div>
   );
 }
