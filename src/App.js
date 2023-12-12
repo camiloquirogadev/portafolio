eeb96dd727c78855a67a36be72f3c3b7e426cce5
Camilo Quiroga <camilosolquiroga@gmail.com>
2023-12-12 08:41:57 -0300

    project

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { NavBar } from './components/NavBar';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Banner } from './components/Banner';
 import { Skills } from './components/Skills';
+import { Project } from './components/Projects';
 
 
 function App() {
