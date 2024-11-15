9e4f4ce8cbfe6c68ca0e0185e5ae4263c63fd533
camilo <camilosolquiroga@gmail.com>
2024-11-14 22:38:22 -0300

    Agregar archivo .gitignore

---
+++ portfolio_backend/settings.py
@@ -0,0 +1,14 @@
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    
+    # Agrega 'rest_framework' aquí
+    'rest_framework',
+
+    # Otras aplicaciones personalizadas de tu proyecto
+    'portfolio_backend',  # Si esta es tu app personalizada
+]
