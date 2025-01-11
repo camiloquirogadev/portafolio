d681aaffa61c9557e82bd97e2df1663f9afb62b9
Camilo Quiroga <camilosolquiroga@gmail.com>
2025-01-10 21:28:06 -0300

    correciones

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import ModernAbout from './components/ModernAbout';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App" basename="/portafolio">
       <Header />  
       <ModernAbout />
       <Services />
