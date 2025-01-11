8fb55808fdeb40b7e49a4d417a257825927ae363
Camilo Quiroga <camilosolquiroga@gmail.com>
2025-01-11 01:54:14 -0300

    update

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom'; // Importar Router
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; // Importar Router y Routes
 import Header from './components/Header';
+import ModernAbout from './components/ModernAbout';
+import Services from './components/Services';
 import Contact from './components/Contact'; // Importar Contact
 import Footer from './components/Footer';
 import './App.css'; // Estilos
-import Services from './components/Services';
-import ModernAbout from './components/ModernAbout';
 
 function App() {
   return (
-    <Router basename="/portafolio"> 
+    <Router basename="/portafolio">
       <div className="App">
-        <Header />  
-        <ModernAbout />
-        <Services />
-        <Contact />
+        <Header />
+       <ModernAbout />
+       <Services />
+       <Contact />
         <Footer />
       </div>
     </Router>
