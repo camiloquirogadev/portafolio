2adca7bc878e7d329e0627b0f027c5d4778a9d61
Camilo Quiroga <camilosolquiroga@gmail.com>
2025-01-10 22:23:01 -0300

    clean

--- portfolio_backend/urls.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include('contact.urls')),  # Incluir la ruta de la API de contacto
-]
